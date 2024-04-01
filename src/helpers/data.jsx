f0c9261bab9955f983875b77becc5fb4f0910369
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-04-01 11:12:28 +0300

    add proje again

---
+++ src/helpers/data.jsx
@@ -0,0 +1,74 @@
+export const appointmentData = [
+  {
+    id: 1,
+    patient: "Barry Vermont",
+    day: new Date(),
+    consulted: true,
+    doctor: "Dr. Hazel Valery",
+  },
+  {
+    id: 2,
+    patient: "Harry Coming",
+    day: new Date(),
+    consulted: false,
+    doctor: "Dr. Jessica White",
+  },
+  {
+    id: 3,
+    patient: "Aaron Black",
+    day: new Date(),
+    consulted: true,
+    doctor: "Dr. Jasmine Carpanter",
+  },
+]
+
+export const doctorData = [
+  {
+    id: 1,
+    name: "Dr.Aidan Verly",
+    dep: "Cardiology",
+    img: "./img/dr1.jpg",
+  },
+  {
+    id: 2,
+    name: "Dr. Laurel Kinsey",
+    dep: "Radiolgy",
+    img: "./img/dr2.jpg",
+  },
+  {
+    id: 3,
+    name: "Dr. Katty Mary",
+    dep: "Orthopedy",
+    img: "./img/dr3.jpg",
+  },
+  {
+    id: 4,
+    name: "Dr. Rane Perla",
+    dep: "Gastroentogoly",
+    img: "./img/dr4.jpg",
+  },
+  {
+    id: 5,
+    name: "Harry Derson",
+    dep: "Surgery",
+    img: "./img/dr9.jpg",
+  },
+  {
+    id: 6,
+    name: "Dr. Valencia Abraham",
+    dep: "Neurology",
+    img: "./img/dr6.jpg",
+  },
+  {
+    id: 7,
+    name: "Dr.Asley Ursula",
+    dep: "Pediatry",
+    img: "./img/dr7.jpg",
+  },
+  {
+    id: 8,
+    name: "Dr. Jenny Freya",
+    dep: "Oncology",
+    img: "./img/dr8.jpg",
+  },
+]
---
+++ src/pages/Home.jsx
@@ -0,0 +1,101 @@
+import AddModal from "../components/AddModal/AddModal";
+import AppointmentList from "../components/AppointmentList/AppointmentList";
+import Doctors from "../components/Doctors/Doctors";
+import { doctorData } from "../helpers/data";
+import { useState, useEffect } from "react";
+import HomeStyle from "./Home.module.css";
+import Swal from "sweetalert2";
+
+const Home = () => {
+  const [selectedDoctor, setSelectedDoctor] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [appointments, setAppointments] = useState(() => {
+    const savedAppointments = localStorage.getItem("appointments") //localstroge dan başlangıç verisi çekme..
+    return savedAppointments ? JSON.parse(savedAppointments) : [] // [] bunu yazmadığımda hata aldım!!
+  })
+
+  //* LOCALSTORAGE
+  useEffect(() => {
+    localStorage.setItem("appointments", JSON.stringify(appointments));
+  })
+  
+  //*DOKTOR SEÇME VE MODALA GÖNDERME
+  const onSelectDoctor = (name) => {
+    setSelectedDoctor(name);
+    setShowModal(true);
+  };
+
+  //* MODAL
+  const handleClose = () => setShowModal(false);
+
+  //* APPOİNTMENT EKLEME
+  const addAppointment = (newAppointment) => {
+    setAppointments([
+      ...appointments,
+      {
+        patientName: `${newAppointment.patientName}`,
+        date: `${newAppointment.date}`,
+      },
+    ]); //! appointment dizisini aç, yeni randevuyu ekle
+  };
+
+  //* APPOİNTMENT SİLME(appointmentden girilien index e göre silme)
+  const removeAppointment = (index) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((res) => {
+      if (res) {
+        setAppointments((appointments) =>
+          appointments.filter((app, i) => i !== index)
+        );
+        Swal.fire("Deleted!", "Appointment has been deleted.", "success");
+      }
+    });
+  }
+
+  //* CONSULTED
+  const toggleConsulted = (index) => {
+    setAppointments((appointments) => appointments.map((appointment,i) => i === index ? {...appointment, consulted: !appointment.consulted} : appointment))
+  }
+  return (
+    <div className={`text-center ${HomeStyle.home}`}>
+      <div className={`d-flex align-items-center gap-2 ${HomeStyle.logo}`}>
+        <img src="img/medical-5459654_1280.png" alt="Logo" />
+        <h1>Deniz Hospital</h1>
+      </div>
+      <div className="border position-relative pt-5 mt-5">
+        <h3 className={`${HomeStyle.ourdoctor}`}>Our Doctors</h3>
+        <div className="doctors-container d-flex flex-wrap gap-5 justify-content-center px-5">
+          {doctorData.map((doctor) => (
+            <Doctors
+              onselectDoctor={onSelectDoctor}
+              key={doctor.id}
+              {...doctor}
+            />
+          ))}
+        </div>
+      </div>
+
+      <AddModal
+        show={showModal}
+        handleClose={handleClose}
+        selectedDoctor={selectedDoctor}
+        addAppointment={addAppointment}
+      />
+      <AppointmentList
+        appointments={appointments}
+        selectedDoctor={selectedDoctor.name}
+        removeAppointment={removeAppointment}
+        handleToggleConsulted={toggleConsulted}
+      />
+    </div>
+  );
+};
+
+export default Home;
